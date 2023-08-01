7e65fe9d70907b8353bae23f5300fb2c06e49747
Moutasim3282 <moutasimamjad3282@gmail.com>
2023-08-01 22:41:48 +0500

    user auth added

--- lib/theme/spaces.dart
+++ lib/theme/spaces.dart
@@ -12,4 +12,4 @@ const extraSmallVerticalSizedBox = SizedBox(width: 5);
 const smallVerticalSizedBox = SizedBox(height: 10);
 const mediumVerticalSizedBox = SizedBox(height: 20);
 const largeVerticalSizedBox = SizedBox(height: 30);
-const extraLargeVerticalSizedBox = SizedBox(height: 40);
+final extraLargeVerticalSizedBox = SizedBox(height: 40);
